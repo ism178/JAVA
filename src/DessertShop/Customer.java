5a457a09317dc8500f1b27e298cf9710108c7344
Jose Yanez <121988875+zenay910@users.noreply.github.com>
2023-02-21 18:03:54 -0700

    customer added

---
+++ src/DessertShop/Customer.java
@@ -0,0 +1,39 @@
+package dessertShop;
+import java.util.*;
+
+public class Customer {
+
+	//Attributes
+	private String custName;
+	private ArrayList<Order>orderHistory;
+	private int custID;
+	private static int nextCustID = 1000;
+	
+	//Constructors
+	public Customer(String cN) {
+		custName = cN;
+		nextCustID = 1000;
+	}
+	
+	//Methods
+	public String getName() {
+		return custName;
+	}
+	
+	public int getID() {
+		return custID;
+	}
+	
+	public ArrayList<Order>getOrderHistory(){
+		return orderHistory;
+	}
+	
+	public void setName(String n) {
+		custName = n;
+	}
+	
+	public void addToHistory(Order o) {
+		
+	}
+	
+}
